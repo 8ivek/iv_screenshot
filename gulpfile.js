47fb53c68de24b65cdf0980bfd8c511b97c54bfb
Bivek <bivek_j@yahoo.com>
2017-01-18 10:48:18 -0500

    package.json file was added

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,20 @@
 //gulpfile.js
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function(){
     // Default task code
     console.log('GULP GULP GULP')
+});
+
+//script paths
+var jsFiles = 'assets/scripts/**/*.js',
+    jsDest = 'dist/scripts';
+
+gulp.task('scripts', function() {
+    return gulp.src(jsFiles)
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest(jsDest));
 });
